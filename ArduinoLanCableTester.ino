0689be4f5d57681b9d34c38580841fd95bc405ea
Robys <giaffy@gmail.com>
2020-12-30 17:11:38 +0100

    set correct file indentation;
    use spaces, 4 spaces for indent;

--- ArduinoLanCableTester.ino
+++ ArduinoLanCableTester.ino
@@ -1,7 +1,7 @@
 //
 // LAN cable tester 
 //
-// update from XXX
+// update from original variecose
 // by roby
 // 20201223
 
@@ -15,14 +15,14 @@
 // note: a delay of 1 msec. is used to allow the console
 //       to finish the output of message
 #define print_debug( X ) do {   \
-    Serial.println( X ); \
-    delay(1);          \
-  } while(0)
+    Serial.println( X );        \
+    delay(1);                   \
+} while(0)
 
 #define print_debugnln( X ) do {   \
-    Serial.print( X ); \
-    delay(1);          \
-  } while(0)
+    Serial.print( X );             \
+    delay(1);                      \
+} while(0)
   
 #else
 //
@@ -38,51 +38,52 @@
 const int pinStraightCable[8] = {2, 3, 4, 5, 6, 7, 8, 9};
 const int pinCrossCable[8] = {4, 7, 2, 5, 6, 3, 8, 9};
 
-void PulseOut(char pinNo, int delayInterval)
-{
-  digitalWrite(pinNo, HIGH);
-  delay(delayInterval);
-  digitalWrite(pinNo, LOW);
+void PulseOut(char pinNo, int delayInterval) {
+
+    digitalWrite(pinNo, HIGH);
+    delay(delayInterval);
+    digitalWrite(pinNo, LOW);
+    return;
 }
 
 void setup() {
-  //
-  // put your setup code here, to run once:
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-//  pinMode(12, INPUT); // Straight (normal) cable or cross cable
-  pinMode(13, OUTPUT); // Control LED
-  Serial.begin(9600);
-  print_debug("LAN cable tester: starting ...");
+    //
+    // put your setup code here, to run once:
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    //  pinMode(12, INPUT); // Straight (normal) cable or cross cable
+
+    pinMode(13, OUTPUT); // Control LED
+
+    Serial.begin(9600);
+    print_debug("LAN cable tester: starting ...");
 }
 
 void loop() {
-
-//
-// Straight (normal) cable
-    print_debug("testing straight LAN cable");
+    //
+    // Straight (normal) cable
+    print_debug("testing straight LAN cable pins ...");
 
     for (int i = 0; i < 8; i++) {
 
-      print_debugnln(i+1);
-      print_debugnln(": line ");
-      print_debugnln(pinStraightCable[i]-1);
-      print_debugnln(" (pin ");
-      print_debugnln(pinStraightCable[i]);
-      print_debug(")");
+        print_debugnln(i+1);
+        print_debugnln(": line ");
+        print_debugnln(pinStraightCable[i]-1);
+        print_debugnln(" (pin ");
+        print_debugnln(pinStraightCable[i]);
+        print_debug(")");
 
-      PulseOut(pinStraightCable[i], 1000);
+        PulseOut(pinStraightCable[i], 1000);
 
-      PulseOut(13, 50);
+        PulseOut(13, 50);
     }
 
     print_debug("waiting 5 secs. ...");
     delay(5000);
-    
- }
+}
